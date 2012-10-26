cea7320a0faced28df6a49ca4e8a95767f93ae6e
Dai Bui <daib@eecs.berkeley.edu>
2012-10-25 22:03:50 -0700

    generate traffic files

--- scripts/optimize.py
+++ scripts/optimize.py
@@ -3,7 +3,19 @@
 import os
 import sys
 import re
+import copy
 
+def write_traffic(name, traffic):
+    FILE = open(name, 'w')
+    for packet in traffic:
+        line = ''
+        for item in packet:
+            line = line + ' ' + str(item)
+        line = line.strip()
+        print>>FILE, line
+        
+    FILE.close()
+    
 path = sys.argv[1]
 
 iterations = 100
@@ -15,7 +27,19 @@ assumed_cpu_freq = OneGhz
 
 os.chdir(path)
 
+start_time = 10 # start sending packets at cycle 10
+packet_flits = 6 # packet size in numbers of flits
+flit_size = 8 #flit size in numbers of bytes
+packet_bytes = packet_flits * flit_size
+max_packet_bytes = packet_flits * flit_size
+
+traffic_idx = 4
+traffic_used_idx = 5
+
 for dir in os.listdir(path):
+    
+    if os.path.isfile('./dir'):
+        continue
     # run to obtain profiling information first
     os.chdir('./' + dir + '/streamit')
     
@@ -23,7 +47,7 @@ for dir in os.listdir(path):
     
     for dim in [4, 6, 8]:
         # compile from str to c
-        os.system("make -f Makefile.mk BACKEND=\'--spacetime --newSimple " + str(dim) + " --i " + str(iterations) + "\'")
+        #os.system("make -f Makefile.mk BACKEND=\'--spacetime --newSimple " + str(dim) + " --i " + str(iterations) + "\'")
         
         # compile the generated c program
         os.system("cp /home/dai/prog/compiler/streamit/tmp/Makefile ./")
@@ -63,7 +87,84 @@ for dir in os.listdir(path):
         
         FILE.close()
         
-        # generate ILP files
+        #generate ILP files
+        
+        #generate wire config
+        
+        #generate routing config
+        
+        #generate traffic
+        dirty_flows = copy.deepcopy(flows) # make a copy of the flows in case it is modified
+        
+        traffics = []
+        #for each source node
+        for x in range(0, dim):
+            for y in range(0,dim):
+                # find flows that have this node as the source node
+                fs = []
+                total_traffic = 0 #total traffic in bytes from this node in one iteration
+                smallest_traffic = float('inf')
+                
+                packets = []    #packets from this source
+                
+                traffics.append([x,y,packets])
+                
+                for f in dirty_flows:
+                    if f[0] == x and f[1] == y:
+                        dirty_flows.remove(f)
+                        f.append(0)  #total traffic used for this flow
+                        fs.append(f)
+                        total_traffic += f[traffic_idx]
+                        if smallest_traffic > f[traffic_idx]:
+                            smallest_traffic = f[traffic_idx]
+                
+                iter = 0
+                while len(fs) > 0:
+                    iter = iter + 1
+                    current_time = iter + start_time
+                    # sequentially send traffic for each flow
+                    for f in fs:
+                        # get the traffic fraction of this flow
+                        max_allow = packet_bytes * f[traffic_idx] * iter / smallest_traffic    # max traffic amount till now for this flow
+                        traffic_left = max_allow - f[traffic_used_idx] # the amount of traffic left till now
+                        
+                        if f[traffic_idx] - max_allow < packet_bytes:   # if the amount of traffic left smaller than one packet
+                            if f[traffic_idx] - max_allow > 0:
+                                packet = copy.deepcopy(f[0:4])
+                                packet.insert(0,current_time)
+                                packet.append(f[traffic_idx] - max_allow)
+                                packets.append(packet)
+                            fs.remove(f)
+                        else:
+                            # convert this amount traffic to numbers of packets
+                            npackets = traffic_left / packet_bytes
+                        
+                            for n in range(0, npackets):
+                                packet = copy.deepcopy(f[0:4])
+                                packet.insert(0,current_time)
+                                packet.append(packet_bytes)
+                                packets.append(packet)
+                                f[traffic_used_idx] = f[traffic_used_idx] + packet_bytes
+                                
+                            
+        #write to files
+        os.system('mkdir traffics')
+        
+        os.chdir('./traffics')
+        packets = []
+        for trace in traffics:
+            write_traffic(dir + '.' + str(trace[0]) + '.' + str(trace[1]), trace[2])
+            packets.extend(trace[2])
+            
+        packets.sort(key=lambda tup:tup[0])
+        write_traffic(dir, packets)
+        
+        #invoke simulation
+        os.system('vnoc ' + dir)
+        os.chdir('../')
+        
+        #collect data
+        
         
     os.chdir("../../")
         
